96f7c376a292948bfeaaa99d580b3b2e91ef1d16
Matej Kuprešak <matej.kupresak@gmail.com>
2020-10-30 17:28:46 +0100

    Finished HW

---
+++ Homework1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Logic;
+
+namespace Homework1
+{
+    class Program
+    {
+        private static Random generator = new Random();
+        private static double currentScore;
+        public static double GenerateRandomScore()  //Pokušao sam ovu funkciju staviti u klasu(zakomentirana je tamo i dalje), ali mi je VS javljao grešku
+        {
+            currentScore = generator.NextDouble() * (10.0 - 0.0) + 0.0;
+            return currentScore;
+        }
+
+        static void Main(string[] args)
+        {
+            Episode ep1, ep2;
+            ep1 = new Episode();
+            ep2 = new Episode(10, 64.39, 8.7);
+
+            int viewers = 10;
+
+            for (int i = 0; i < viewers; i++)
+            {
+                ep1.AddView(GenerateRandomScore());
+                Console.WriteLine(Math.Round(ep1.GetMaxScore(), 5));
+            }
+
+            if(ep1.GetAverageScore() > ep2.GetAverageScore())
+            {
+                Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+            }
+            else
+            {
+                Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+            }
+        }
+    }
+}
---
+++ Logic/ClassEpisode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Logic
+{
+    public class Episode
+    {
+        private int viewers;
+        private double scoreSum;
+        private double maxScore;
+        private double averageScore;
+        //private static Random generator = new Random();
+        //private static double currentScore;
+
+        public int GetViewerCount() { return this.viewers; }
+        public double GetScoreSum() { return this.scoreSum; }
+        public double GetMaxScore() { return this.maxScore; }
+
+        public void SetViewerCount(int viewers) { this.viewers = viewers; }
+        public void SetScoreSum(double scoreSum) { this.scoreSum = scoreSum; }
+        public void SetMaxScore(double maxScore) { this.maxScore = maxScore; }
+
+        public Episode ()
+        {
+
+        }
+        public Episode(int viewers, double scoreSum, double maxScore)
+        {
+            this.viewers = viewers;
+            this.scoreSum = scoreSum;
+            this.maxScore = maxScore;
+        }
+
+        public void AddView(double currentScore)
+        {
+            this.scoreSum += currentScore;
+
+            if(currentScore > maxScore)
+            {
+                this.maxScore = currentScore;
+            }
+
+            this.viewers += 1;
+        }
+
+        public double GetAverageScore()
+        {
+            this.averageScore = this.scoreSum / this.viewers;
+            return averageScore;
+        }
+
+        //public static double GenerateRandomScore()
+        //{
+        //    currentScore = generator.NextDouble() * (10.0 - 0.0) + 0.0;
+        //    return currentScore;
+        //}
+    }
+}
